d1df9019d2f727939503618cde70fd688c70272e
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-06-05 13:19:58 +0200

    Create calculator object & test

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function createCalculator () {
+  const calculator = {
+    add(a, b) {
+      return a + b;
+    },
+    subtract(a, b) {
+      return a - b;
+    },
+    multiply(a, b) {
+      return a * b;
+    },
+    divide(a, b) {
+      return a / b;
+    },
+  }
+  return calculator
+};
+
+module.exports = createCalculator;
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,7 @@
 const capitalize = require('./capitalize');
-const reverse = require('./reverse-string')
+const reverse = require('./reverse-string');
+const calculator = require('./calculator');
+const createCalculator = require('./calculator');
 
 test('capitalize only the first letter of a string', () => {
   expect(capitalize('heLLo WORlD')).toBe('Hello world');
@@ -7,4 +9,11 @@ test('capitalize only the first letter of a string', () => {
 
 test('reverse a string', () => {
   expect(reverse('Hello world')).toBe('dlrow olleH');
+});
+
+test('take numbers and perform correct operation', () => {
+  expect(createCalculator().add(2,1)).toBe(3);
+  expect(createCalculator().subtract(7,4)).toBe(3);
+  expect(createCalculator().multiply(3,6)).toBe(18);
+  expect(createCalculator().divide(14,7)).toBe(2);
 });
