43daa5c5b0aff77d0f62f61005340d8dca54508c
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-06-05 13:03:13 +0200

    Change capitalize test file to include all tests

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,10 @@
 const capitalize = require('./capitalize');
+const reverse = require('./reverse-string')
 
 test('capitalize only the first letter of a string', () => {
   expect(capitalize('heLLo WORlD')).toBe('Hello world');
+});
+
+test('reverse a string', () => {
+  expect(reverse('Hello world')).toBe('dlrow olleH');
 });
